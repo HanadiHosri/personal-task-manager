141aed6e952c463adaf130d5d4eaa3b9b25494ea
unknown <hanadi.hosri@gmail.com>
2024-04-16 21:00:26 +0300

    adding config and index.js

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { connect } = require("./config/db.config");
+const app = express();
+app.use(express.json());
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+
+
+app.listen(PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Server is listening on port ${PORT}`);
+    connect();
+});
